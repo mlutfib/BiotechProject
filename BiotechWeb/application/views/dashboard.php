1db4e81ed319403d967a0682e6a84962abf5286d
Dimas R. Danu <dimasdanz@gmail.com>
2014-02-04 17:00:51 +0700

    Dashboard two

--- BiotechWeb/application/views/dashboard.php
+++ BiotechWeb/application/views/dashboard.php
@@ -5,187 +5,169 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-sm-8">
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<i class="fa fa-unlock-alt"></i> Door Control
-						</div>
-						<div class="panel-body">
-							<div class="table-responsive">
-								<table class="table table-hover table-condensed">
-									<thead>
-										<tr>
-											<th>No.</th>
-											<th>Name</th>
-											<th>Time</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>1. </td>
-											<td>Dimas</td>
-											<td>13:05:59</td>
-										</tr>
-										<tr>
-											<td>2. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>3. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>4. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>5. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>6. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>7. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>8. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>9. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-										<tr>
-											<td>10. </td>
-											<td>Ganteng</td>
-											<td>14:56:59</td>
-										</tr>
-									</tbody>
-								</table>
+		<div class="col-sm-12">
+			<div class="col-sm-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<i class="fa fa-home fa-fw"></i> Home Control
+					</div>
+					<div class="panel-body text-center">
+						<div class="row">
+							<div class="col-sm-3">
+								<h5>Lamp 1</h5>
+								<h2>ON</h2>
 							</div>
-						</div>
-					</div>			
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<i class="fa fa-leaf"></i> Greenhouse
-						</div>
-						<div class="panel-body text-center">
-							<h4>Plant Setting: Weed</h4>
-							<div class="row">
-								<div class="col-sm-4">
-									<div class="well well-sm">
-										<h5>Lux</h5>
-										<h2>6000 lx</h2>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="well well-sm">
-										<h5>Humidity</h5>
-										<h2>Humid</h2>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="well well-sm">
-										<h5>Temperature</h5>
-										<h2>32&deg;C</h2>
-									</div>
-								</div>
+							<div class="col-sm-3">
+								<h5>Lamp 2</h5>
+								<h2>OFF</h2>
+							</div>
+							<div class="col-sm-3">
+								<h5>Lamp 3</h5>
+								<h2>OFF</h2>
+							</div>
+							<div class="col-sm-3">
+								<h5>Lamp 4</h5>
+								<h2>ON</h2>
 							</div>
 						</div>
 					</div>
 				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<i class="fa fa-tint fa-fw"></i> Water Monitoring
+					</div>
+					<div class="panel-body text-center">
+						<div class="row">
+							<div class="col-sm-6">
+								<h5>Level</h5>
+								<h2>120 CM</h2>
+							</div>
+							<div class="col-sm-6">
+								<h5>Turbidity</h5>
+								<h2>0%</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-2">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<i class="fa fa-cogs fa-fw"></i> Server Room
+					</div>
+					<div class="panel-body text-center">
+						<h5>Temperature</h5>
+						<h2>38&deg;C</h2>
+					</div>
+				</div>	
 			</div>
 		</div>
-		<div class="col-sm-4">
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<i class="fa fa-cogs fa-fw"></i> Server Room
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="col-sm-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<i class="fa fa-unlock-alt"></i> Door Control
+					</div>
+					<div class="panel-body">
+						<div class="table-responsive">
+							<table class="table table-hover table-condensed">
+								<thead>
+									<tr>
+										<th>No.</th>
+										<th>Name</th>
+										<th>Time</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1. </td>
+										<td>Dimas</td>
+										<td>13:05:59</td>
+									</tr>
+									<tr>
+										<td>2. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>3. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>4. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>5. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>6. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>7. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>8. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>9. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+									<tr>
+										<td>10. </td>
+										<td>Ganteng</td>
+										<td>14:56:59</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
-						<div class="panel-body text-center">
-							<h5>Temperature</h5>
-							<h2>38&deg;C</h2>
-						</div>
-					</div>	
+					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<i class="fa fa-tint fa-fw"></i> Water Monitoring
-						</div>
-						<div class="panel-body text-center">
-							<div class="row">
-								<div class="col-sm-6">
-									<div class="well well-sm">
-										<h5>Level</h5>
-										<h2>120 CM</h2>
-									</div>
+			<div class="col-sm-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<i class="fa fa-leaf"></i> Greenhouse
+					</div>
+					<div class="panel-body text-center">
+						<h4>Plant Setting: Weed</h4>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="well well-sm">
+									<h5>Lux</h5>
+									<h2>6000 lx</h2>
 								</div>
-								<div class="col-sm-6">
-									<div class="well well-sm">
-										<h5>Turbidity</h5>
-										<h2>0%</h2>
-									</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="well well-sm">
+									<h5>Humidity</h5>
+									<h2>Humid</h2>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="well well-sm">
+									<h5>Temperature</h5>
+									<h2>32&deg;C</h2>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>	
-			</div>
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<i class="fa fa-home fa-fw"></i> Home Control
-						</div>
-						<div class="panel-body text-center">
-							<div class="row">
-								<div class="col-sm-12">
-									<h5>Lamp 1</h5>
-									<h2>ON</h2>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-sm-12">
-									<h5>Lamp 2</h5>
-									<h2>OFF</h2>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-sm-12">
-									<h5>Lamp 3</h5>
-									<h2>OFF</h2>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-sm-12">
-									<h5>Lamp 4</h5>
-									<h2>OFF</h2>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>	
+				</div>
 			</div>
 		</div>
 	</div>
