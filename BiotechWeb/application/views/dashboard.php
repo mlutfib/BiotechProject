f0f395f25b05ee132ca6bc601cb3e5f4f2b78eb2
Dimas R. Danu <dimasdanz@gmail.com>
2014-02-04 16:33:00 +0700

    Nesting column

--- BiotechWeb/application/views/dashboard.php
+++ BiotechWeb/application/views/dashboard.php
@@ -8,74 +8,67 @@
 	</div>
 	<div class="row">
 		<div class="col-sm-8">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<i class="fa fa-unlock-alt"></i> Door Control
-				</div>
-				<div class="panel-body">
-					<div class="table-responsive">
-						<table class="table table-hover table-condensed">
-							<thead>
-								<tr>
-									<th>No.</th>
-									<th>Name</th>
-									<th>Time</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1. </td>
-									<td>Dimas</td>
-									<td>13:05:59</td>
-								</tr>
-								<tr>
-									<td>2. </td>
-									<td>Ganteng</td>
-									<td>14:56:59</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>			
-		</div>
-		<div class="col-sm-4">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<i class="fa fa-cogs fa-fw"></i> Server Room
-				</div>
-				<div class="panel-body text-center">
-					<h5>Temperature</h5>
-					<h2>38&deg;C</h2>
-				</div>
-			</div>			
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-8">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<i class="fa fa-leaf"></i> Greenhouse
-				</div>
-				<div class="panel-body text-center">
-					<h4>Plant Setting: Weed</h4>
-					<div class="row">
-						<div class="col-sm-4">
-							<div class="well well-sm">
-								<h5>Lux</h5>
-								<h2>6000 lx</h2>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<i class="fa fa-unlock-alt"></i> Door Control
+						</div>
+						<div class="panel-body">
+							<div class="table-responsive">
+								<table class="table table-hover table-condensed">
+									<thead>
+										<tr>
+											<th>No.</th>
+											<th>Name</th>
+											<th>Time</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>1. </td>
+											<td>Dimas</td>
+											<td>13:05:59</td>
+										</tr>
+										<tr>
+											<td>2. </td>
+											<td>Ganteng</td>
+											<td>14:56:59</td>
+										</tr>
+									</tbody>
+								</table>
 							</div>
 						</div>
-						<div class="col-sm-4">
-							<div class="well well-sm">
-								<h5>Humidity</h5>
-								<h2>Humid</h2>
-							</div>
+					</div>			
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<i class="fa fa-leaf"></i> Greenhouse
 						</div>
-						<div class="col-sm-4">
-							<div class="well well-sm">
-								<h5>Temperature</h5>
-								<h2>32&deg;C</h2>
+						<div class="panel-body text-center">
+							<h4>Plant Setting: Weed</h4>
+							<div class="row">
+								<div class="col-sm-4">
+									<div class="well well-sm">
+										<h5>Lux</h5>
+										<h2>6000 lx</h2>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="well well-sm">
+										<h5>Humidity</h5>
+										<h2>Humid</h2>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="well well-sm">
+										<h5>Temperature</h5>
+										<h2>32&deg;C</h2>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -83,27 +76,44 @@
 			</div>
 		</div>
 		<div class="col-sm-4">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<i class="fa fa-tint fa-fw"></i> Water Monitoring
-				</div>
-				<div class="panel-body text-center">
-					<div class="row">
-						<div class="col-sm-6">
-							<div class="well well-sm">
-								<h5>Level</h5>
-								<h2>120 CM</h2>
-							</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<i class="fa fa-cogs fa-fw"></i> Server Room
 						</div>
-						<div class="col-sm-6">
-							<div class="well well-sm">
-								<h5>Turbidity</h5>
-								<h2>0%</h2>
+						<div class="panel-body text-center">
+							<h5>Temperature</h5>
+							<h2>38&deg;C</h2>
+						</div>
+					</div>	
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<i class="fa fa-tint fa-fw"></i> Water Monitoring
+						</div>
+						<div class="panel-body text-center">
+							<div class="row">
+								<div class="col-sm-6">
+									<div class="well well-sm">
+										<h5>Level</h5>
+										<h2>120 CM</h2>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<div class="well well-sm">
+										<h5>Turbidity</h5>
+										<h2>0%</h2>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>			
+				</div>	
+			</div>
 		</div>
 	</div>
 </div>
